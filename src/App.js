0e33c29bdc8620c23d133e7903b03b32840ba16f
Poojakadam23 <poojakiran660@gmail.com>
2023-06-13 17:37:46 +0530

    static website

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
+import DataBlock from "./DataBlock";
+
 function App() {
   return (
     <>
       <h1 className="bg-primary text-light p-3">My Project</h1>
-
-      <div className="alert alert-primary">
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
-        laborum minima doloremque tempora in alias delectus voluptatum ipsum.
-        Deleniti optio ad rem provident esse, veritatis ipsam incidunt. Illum,
-        explicabo omnis. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Provident esse tenetur blanditiis voluptatibus ex ab, ad in harum
-        nostrum ipsam quaerat quia dolore suscipit id consequatur quis delectus
-        neque animi?
-      </div>
-
-      <div className="alert alert-primary">
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
-        laborum minima doloremque tempora in alias delectus voluptatum ipsum.
-        Deleniti optio ad rem provident esse, veritatis ipsam incidunt. Illum,
-        explicabo omnis. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Provident esse tenetur blanditiis voluptatibus ex ab, ad in harum
-        nostrum ipsam quaerat quia dolore suscipit id consequatur quis delectus
-        neque animi?
-      </div>
-
-      <div className="alert alert-primary">
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
-        laborum minima doloremque tempora in alias delectus voluptatum ipsum.
-        Deleniti optio ad rem provident esse, veritatis ipsam incidunt. Illum,
-        explicabo omnis. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Provident esse tenetur blanditiis voluptatibus ex ab, ad in harum
-        nostrum ipsam quaerat quia dolore suscipit id consequatur quis delectus
-        neque animi?
-      </div>
+      <DataBlock />
+      <DataBlock />
+      <DataBlock />
     </>
   );
 }
 
 export default App;
-
-  
-
