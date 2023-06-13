8df49254d160d540738cbd3ef6b2b83aecc63e00
Poojakadam23 <poojakiran660@gmail.com>
2023-06-13 16:07:24 +0530

    ternanry operator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+function App() {
+  let jsx1 = <h1>Good Morning</h1>;
+  let jsx2 = <h1>Good Night</h1>;
 
+  return false ? jsx1 : jsx2;
+}
 
-  function App() {
-    // jsx single liner :: with optional parenthesis
-    let jsx1 = <h1>Hello Universe</h1>;
-  
-    return jsx1;
-  }
-  
-  export default App;
+export default App;
+
   
 
