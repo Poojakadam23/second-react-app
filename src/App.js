bd950ba70c28930f9155654943b7d5eb973b2489
Poojakadam23 <poojakiran660@gmail.com>
2023-06-13 17:31:56 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 function App() {
-  let jsx1 = <h1>Good Morning</h1>;
-  let jsx2 = <h1>Good Night</h1>;
+  return (
+    <>
+      <h1 className="bg-primary text-light p-3">My Project</h1>
 
-  return false ? jsx1 : jsx2;
+      <div className="alert alert-primary">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+        laborum minima doloremque tempora in alias delectus voluptatum ipsum.
+        Deleniti optio ad rem provident esse, veritatis ipsam incidunt. Illum,
+        explicabo omnis. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Provident esse tenetur blanditiis voluptatibus ex ab, ad in harum
+        nostrum ipsam quaerat quia dolore suscipit id consequatur quis delectus
+        neque animi?
+      </div>
+
+      <div className="alert alert-primary">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+        laborum minima doloremque tempora in alias delectus voluptatum ipsum.
+        Deleniti optio ad rem provident esse, veritatis ipsam incidunt. Illum,
+        explicabo omnis. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Provident esse tenetur blanditiis voluptatibus ex ab, ad in harum
+        nostrum ipsam quaerat quia dolore suscipit id consequatur quis delectus
+        neque animi?
+      </div>
+
+      <div className="alert alert-primary">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+        laborum minima doloremque tempora in alias delectus voluptatum ipsum.
+        Deleniti optio ad rem provident esse, veritatis ipsam incidunt. Illum,
+        explicabo omnis. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Provident esse tenetur blanditiis voluptatibus ex ab, ad in harum
+        nostrum ipsam quaerat quia dolore suscipit id consequatur quis delectus
+        neque animi?
+      </div>
+    </>
+  );
 }
 
 export default App;
