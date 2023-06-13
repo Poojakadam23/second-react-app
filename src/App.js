e3845e484f809734084cfa0d6006a747aa62faed
Poojakadam23 <poojakiran660@gmail.com>
2023-06-13 15:20:59 +0530

    lorem

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <h1>HELLO WORLD</h1>
+  return (
+  <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+  mollitia exercitationem veritatis harum illum accusantium earum laborum
+  atque natus a qui repudiandae sapiente quae similique, impedit dolor, eius
+  facere laboriosam.
+  </h1>
+  );
+  
 }
 
 export default App;
