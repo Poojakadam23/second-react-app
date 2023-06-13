037557ba12f1245aa9379b3e98458d0c1af96a89
Poojakadam23 <poojakiran660@gmail.com>
2023-06-13 15:45:15 +0530

    hiiii

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
+
 
 function App() {
   return (
+    <>
   <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
   mollitia exercitationem veritatis harum illum accusantium earum laborum
   atque natus a qui repudiandae sapiente quae similique, impedit dolor, eius
   facere laboriosam.
   </h1>
+  </>
   );
   
 }
